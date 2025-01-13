e268bcb6113678a685429c9217fc8e66b37fd5a4
GabrielAlvesGM <lgabrielalvesgm@gmail.com>
2025-01-13 18:15:52 -0300

    Header criado e implementado na aplicação. modificações em tamanhos na main.

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,18 +1,19 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, StyleSheet, TouchableOpacity, Alert, Image, ScrollView } from 'react-native';
-import { SelectList } from 'react-native-dropdown-select-list'; // lista de seleção.
-import RadioGroup from 'react-native-radio-buttons-group'; // Botões de seleção.
-import * as ImagePicker from 'expo-image-picker'; // Expo Image Picker
+import { View, Text, TextInput, StyleSheet, TouchableOpacity, Alert, Image, ScrollView, Fontsize, StatusBar} from 'react-native';
+import { SelectList } from 'react-native-dropdown-select-list'; // Select List
+import RadioGroup from 'react-native-radio-buttons-group'; //  Botões de select
+import * as ImagePicker from 'expo-image-picker'; //Img picker
+import Header from '../../components/Header'; //Header
 
 export default function App() {
     const [selected, setSelected] = useState("");
-    const [imageUri, setImageUri] = useState(null); // Estado para armazenar a URI da imagem selecionada
+    const [imageUri, setImageUri] = useState(null);
     const [radioButtons, setRadioButtons] = useState([
         { id: '1', label: 'Kilograma (Kg)', value: 'Kg', selected: true },
         { id: '2', label: 'Grama (g)', value: 'g' },
     ]);
 
-    const data = [ // lista de seleção.
+    const data = [
         { key: '1', value: 'Fruta' },
         { key: '2', value: 'Vegetal' },
         { key: '3', value: 'Carne' },
@@ -21,85 +22,96 @@ export default function App() {
         { key: '6', value: 'Lanche' },
     ];
 
-    const handleSubmit = () => { // Alerta do botão de enviar.
+    const handleSubmit = () => { 
         Alert.alert('Formulário Enviado', 'Seu cadastro foi enviado com sucesso!');
     };
 
     const handleSelectImage = async () => {
-        // Solicitar permissão para acessar a galeria
         const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
         if (!permissionResult.granted) {
             Alert.alert('Permissão Necessária', 'Precisamos da sua permissão para acessar a galeria.');
             return;
         }
 
-        // Abrir a galeria para selecionar uma imagem
         const result = await ImagePicker.launchImageLibraryAsync({
             mediaTypes: ImagePicker.MediaTypeOptions.Images,
-            quality: 1, // Qualidade máxima
+            quality: 1,
         });
 
         if (!result.canceled) {
-            setImageUri(result.assets[0].uri); // Salvar a URI da imagem selecionada
+            setImageUri(result.assets[0].uri);
         }
     };
 
     return (
+        <ScrollView>
+            <Header title="" onMenuPress={() => console.log('Menu pressionado')} />
 
-        <View style={styles.container}>
-            <Text style={styles.title}>Cadastro de produtos</Text>
+            <View style={styles.container}>
+                <Text style={styles.title}>Cadastro de produtos</Text>
 
-            {/* Botão que abre a galeria e exibe a preview se houver uma imagem */}
-            <View style={styles.vimagepicker}>
-            <TouchableOpacity style={styles.imagePicker} onPress={handleSelectImage}>
-                {imageUri ? (
-                    <Image source={{ uri: imageUri }} style={styles.previewImage} />
-                ) : (
-                    <Image
-                            source={require('../../assets/images/iconeuploadarquivos.jpg')} // Caminho da imagem ilustrativa
-                            style={styles.placeholderImage}
-                        />
-                )}
-            </TouchableOpacity>
-            </View>
-            <Text style={styles.subTitle}>Nome do produto:</Text>
-            <TextInput style={styles.input} />
+                <View style={styles.vimagepicker}>
+                    <TouchableOpacity style={styles.imagePicker} onPress={handleSelectImage}>
+                        {imageUri ? (
+                            <Image source={{ uri: imageUri }} style={styles.previewImage} />
+                        ) : (
+                            <Image
+                                source={require('../../assets/images/iconeuploadarquivos.jpg')}//Path para o icone
+                                style={styles.placeholderImage}
+                            />
+                        )}
+                    </TouchableOpacity>
+                </View>
+                <Text style={styles.subTitle}>Nome do produto:</Text>
+                <TextInput style={styles.input} />
 
-            <Text style={styles.subTitle}>Selecione uma categoria:</Text>
-            <SelectList
+                <Text style={styles.subTitle}>Selecione uma categoria:</Text>
+                <SelectList
                 setSelected={(val) => setSelected(val)}
                 data={data}
                 save="value"
                 boxStyles={styles.dropdown}
                 SelectList={styles.selectLi}
-            />
-            {/* Botão  */}
-            <Text style={styles.subTitle}>Preço:</Text>
-            <TextInput style={styles.input} />
+                />
+                {/* Botão  */}
+                <Text style={styles.subTitle}>Preço:</Text>
+                <TextInput style={styles.input} 
+                placeholder="R$: "
+                keyboardType="numeric"
+                placeholderTextColor="#888"
+                />
 
-            <Text style={styles.subTitle}>Selecione a unidade de medida para a venda:</Text>
-            <RadioGroup
+                <Text style={styles.subTitle}>Selecione a unidade de medida para a venda:</Text>
+                <RadioGroup
                 radioButtons={radioButtons}
                 onPress={(buttonsArray) => setRadioButtons(buttonsArray)}
                 containerStyle={styles.radioGroup}
-            />
+                />
 
-            {/* Texto valor máximo */}
-            <Text style={styles.subTitle}>Valor mínimo por venda:</Text>
-            <TextInput style={styles.input} />
+                {/* Texto valor mínimo */}
+                <Text style={styles.subTitle}
+                >Valor mínimo por venda:</Text>
+                <TextInput style={styles.inputPreco} 
+                placeholder="R$: "
+                keyboardType="numeric"
+                placeholderTextColor="#888"
+                />
 
+                {/* Texto valor máximo */}
+                <Text style={styles.subTitle}
+                >Valor máximo por venda:</Text>
+                <TextInput style={styles.inputPreco} 
+                placeholder="R$: "
+                keyboardType="numeric"
+                placeholderTextColor="#888"
+                />
 
-            {/* Texto valor máximo */}
-            <Text style={styles.subTitle}>Valor máximo por venda:</Text>
-            <TextInput style={styles.input} />
-
-
-
-            {/* Botão Enviar */}
-            <TouchableOpacity style={styles.button} onPress={handleSubmit}>
-                <Text style={styles.buttonText}>Enviar</Text>
-            </TouchableOpacity>
-        </View>
+                {/* Botão Enviar */}
+                <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+                    <Text style={styles.buttonText}>Enviar</Text>
+                </TouchableOpacity>
+            </View>
+        </ScrollView>
     );
 }
 
@@ -110,12 +122,20 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
     },
     title: {
+        fontWeight:"bold",
         marginLeft: 45,
         marginTop: 25,
-        fontSize: 25,
-        marginBottom: 10,
+        fontSize: 30,
+        marginBottom: 25,
     },
     input: {
+        borderWidth: 1,
+        borderColor: '#000',
+        padding: 8,
+        marginBottom: 25,
+        borderRadius: 6,
+    },
+    inputPreco: {
         borderWidth: 1,
         borderColor: '#000',
         padding: 8,
@@ -149,8 +169,8 @@ const styles = StyleSheet.create({
         textAlign: 'center',
     },
     imagePicker: {
-        width: 130,
-        height: 130,
+        width: 200,
+        height: 200,
         backgroundColor: '#ccc',
         justifyContent: 'center',
         alignItems: 'center',
@@ -172,9 +192,8 @@ const styles = StyleSheet.create({
         borderRadius: 3,
     },
     vimagepicker: {
-        flex:1,
-        alignContent:"center",
-        alignItems:"center",
-        
+        flex: 1,
+        alignContent: "center",
+        alignItems: "center",
     }
 });
---
+++ components/Header.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, StatusBar } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+const Header = ({ title, onMenuPress }) => {
+    return (
+        <>
+            <StatusBar backgroundColor="#7D837B"/>
+            <View style={styles.header}>
+                <TouchableOpacity style={styles.menuButton} onPress={onMenuPress}>
+                    <Icon name="menu" size={35} color="#000" />
+                </TouchableOpacity>
+            </View>
+        </>
+    );
+};
+
+const styles = StyleSheet.create({
+    header: {
+        backgroundColor: '#B2FC97',
+        paddingTop: StatusBar.currentHeight,
+        paddingBottom: 10,
+        paddingLeft: 15,
+        paddingRight: 15,
+        height: 75,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    menuButton: {
+        marginLeft: 10,
+    },
+    statusbar: {
+        backgroundColor: "#7D837B",
+    }
+});
+export default Header;
